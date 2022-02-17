9d62f135a27e3ccf7771e232c9d3ad458d0d9a4a
sssloba <slobodan.stojakov@gmail.com>
2022-02-17 23:01:17 +0100

    stepper-controls-builder

--- lib/screens/steppers.dart
+++ lib/screens/steppers.dart
@@ -43,6 +43,48 @@ class _SteppersState extends State<Steppers> {
                 currentStep: currentStepIndex,
                 onStepTapped: (int stepIndex) =>
                     setState(() => currentStepIndex = stepIndex),
+                controlsBuilder: isVertical
+                    ? null // if null default CONTINUE and CANCEL buttons will appear
+                    : (_, __) {
+                        // set BuildContext and ContolsDetails as parameters if needed in custom widgets
+                        //***EXAMPLE***/
+                        //controlsBuilder: (BuildContext context, {VoidCallback? onStepContinue, VoidCallback? onStepCancel}){
+                        //                    return Row(
+                        //   children: <Widget>[
+                        //     TextButton(
+                        //       onPressed: onStepContinue,
+                        //       child: const Text('NEXT'),
+                        //     ),
+                        //     TextButton(
+                        //       onPressed: onStepCancel,
+                        //       child: const Text('EXIT'),
+                        //     ),
+                        //   ],
+                        // );
+                        //}
+                        return const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 24.0),
+                          child: Text(
+                            'You can set your custom widgets in controlsBuilder parameter inside of Stepper widget if you want to modifiy default CONTINUE and CANCEL buttons',
+                            softWrap: true,
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.cyan),
+                          ),
+                        );
+                      },
+                onStepContinue: () {
+                  if (currentStepIndex < 2) {
+                    setState(() => currentStepIndex++);
+                  }
+                },
+                onStepCancel: () {
+                  if (currentStepIndex > 0) {
+                    setState(() => currentStepIndex--);
+                  }
+                },
                 steps: <Step>[
                   const Step(
                     isActive: true,
