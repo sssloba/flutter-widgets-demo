d1932de8d58bfee0bb578995c67bc8e45d2f947e
sssloba <slobodan.stojakov@gmail.com>
2022-02-17 19:00:28 +0100

    switch-direction-button

--- lib/screens/steppers.dart
+++ lib/screens/steppers.dart
@@ -10,6 +10,8 @@ class Steppers extends StatefulWidget {
 
 class _SteppersState extends State<Steppers> {
   int currentStepIndex = 0;
+  bool isVertical = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,26 +19,48 @@ class _SteppersState extends State<Steppers> {
       appBar: AppBar(
         title: const Text('Steppers'),
       ),
-      body: Center(
-        child: Stepper(
-          currentStep: currentStepIndex,
-          onStepTapped: (int stepIndex) =>
-              setState(() => currentStepIndex = stepIndex),
-          steps: <Step>[
-            const Step(
-              isActive: true,
-              title: Text('Start'),
-              content: Text('This is the first step'),
+      body: Theme(
+        data: Theme.of(context).copyWith(
+            colorScheme: ColorScheme.light(
+                primary: isVertical ? Colors.green : Colors.red)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: (() => setState(() => isVertical = !isVertical)),
+                child: Text(isVertical
+                    ? 'Switch to horizontal stepper'
+                    : 'Switch to vertical stepper'),
+              ),
             ),
-            Step(
-              isActive: currentStepIndex >= 1,
-              title: const Text('Middle'),
-              content: const Text('This is the second step'),
-            ),
-            Step(
-              isActive: currentStepIndex == 2,
-              title: const Text('End'),
-              content: const Text('This is the last step'),
+            Expanded(
+              child: Stepper(
+                type:
+                    isVertical ? StepperType.vertical : StepperType.horizontal,
+                currentStep: currentStepIndex,
+                onStepTapped: (int stepIndex) =>
+                    setState(() => currentStepIndex = stepIndex),
+                steps: <Step>[
+                  const Step(
+                    isActive: true,
+                    title: Text('Start'),
+                    content: Text('This is the first step'),
+                  ),
+                  Step(
+                    isActive: currentStepIndex >= 1,
+                    title: const Text('Middle'),
+                    content: const Text('This is the second step'),
+                  ),
+                  Step(
+                    isActive: currentStepIndex == 2,
+                    title: const Text('End'),
+                    content: const Text('This is the last step'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
