3a56cede4208fbde48db4a16569fbc60cdb9e27d
sssloba <slobodan.stojakov@gmail.com>
2024-08-07 21:07:32 +0200

    screen-exports-file

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets_demo/screens/animated_cross_fade_demo.dart';
-import 'package:flutter_widgets_demo/screens/bezier_curves_demo.dart';
-import 'package:flutter_widgets_demo/screens/callback_text_first.dart';
-import 'package:flutter_widgets_demo/screens/custom_buttons_demo.dart';
-import 'package:flutter_widgets_demo/screens/draggable_scrollabe_demo.dart';
-import 'package:flutter_widgets_demo/screens/drop_down_demo.dart';
-import 'package:flutter_widgets_demo/screens/overflow_bar_demo.dart';
-import 'package:flutter_widgets_demo/screens/overlay_portal_demo.dart';
-import 'package:flutter_widgets_demo/screens/paginated_infinite_list_view.dart';
-import 'package:flutter_widgets_demo/screens/physical_model_demo.dart';
-import 'package:flutter_widgets_demo/screens/draggables.dart';
-import 'package:flutter_widgets_demo/screens/home_page.dart';
-import 'package:flutter_widgets_demo/screens/reorderables.dart';
-import 'package:flutter_widgets_demo/screens/slivers.dart';
-import 'package:flutter_widgets_demo/screens/stateful_builder_demo.dart';
-import 'package:flutter_widgets_demo/screens/steppers.dart';
-import 'package:flutter_widgets_demo/screens/value_notifier_draggable.dart';
-import 'package:flutter_widgets_demo/widgets/drawer_tile.dart';
+import 'package:flutter_widgets_demo/screen_exports.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
