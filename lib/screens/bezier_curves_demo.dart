a23eaf34fa609b983ae4db8466dc2e1a92c3ac2d
sssloba <slobodan.stojakov@gmail.com>
2024-08-07 21:04:52 +0200

    custom-clip-with-shapemaker

--- lib/screens/bezier_curves_demo.dart
+++ lib/screens/bezier_curves_demo.dart
@@ -11,24 +11,56 @@ class BezierCurvesDemo extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Bezier Curves Demo'),
       ),
-      body: ClipPath(
-        clipper: _CustomClipPath(),
-        child: Container(
-          color: Colors.green,
-          height: 400,
-          width: 300,
-          child: const Center(
-            child: Text(
-              'Bezier Curve with Clip Path',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 32,
+      body: Column(
+        children: [
+          ClipPath(
+            clipper: _CustomClipPath(),
+            child: Container(
+              color: Colors.green,
+              height: 400,
+              width: 300,
+              child: const Center(
+                child: Text(
+                  'Bezier Curve with Clip Path',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                  ),
+                ),
               ),
             ),
           ),
-        ),
+          Stack(
+            children: [
+              ClipPath(
+                clipper: _CustomClipPathFromShapeMaker(),
+                child: Container(
+                  color: Colors.orange,
+                  height: 300,
+                  width: 300,
+                ),
+              ),
+              const SizedBox(
+                height: 120,
+                width: 200,
+                child: Center(
+                  child: Text(
+                    'Custom Clip with Shapemaker',
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -60,6 +92,28 @@ class _CustomClipPath extends CustomClipper<Path> {
     path.lineTo(w, 0); // 8. point
     path.close();
 
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+// custom clip created from:
+// https://fluttershapemaker.com/
+class _CustomClipPathFromShapeMaker extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width * 0.3180833, size.height * 0.3352429);
+    path.lineTo(size.width * 0.0613333, size.height * 0.9655857);
+    path.quadraticBezierTo(size.width * 0.2744917, size.height * 0.6456286,
+        size.width * 0.7998167, size.height * 0.8452571);
+    path.quadraticBezierTo(size.width * 0.3791417, size.height * 0.6056000,
+        size.width * 0.3180833, size.height * 0.3352429);
+    path.close();
     return path;
   }
 
