d37009356cfb3e2a6bf090d695c9a3e3f28b5a14
sssloba <slobodan.stojakov@gmail.com>
2024-11-07 12:00:44 +0100

    list-wheel-parameters

--- lib/screens/list_wheel_demo.dart
+++ lib/screens/list_wheel_demo.dart
@@ -17,40 +17,48 @@ class _ListWheelDemoState extends State<ListWheelDemo> {
         title: const Text('List Wheel Demo'),
       ),
       body: Center(
-        child: ListWheelScrollView(itemExtent: 200, children: [
-          Container(
-            width: 200,
-            color: Colors.red,
-          ),
-          Container(
-            width: 700,
-            color: Colors.blue,
-          ),
-          Container(
-            width: 200,
-            color: Colors.green,
-          ),
-          Container(
-            width: 300,
-            color: Colors.yellow,
-          ),
-          Container(
-            width: 200,
-            color: Colors.red,
-          ),
-          Container(
-            width: 100,
-            color: Colors.blue,
-          ),
-          Container(
-            width: 50,
-            color: Colors.green,
-          ),
-          Container(
-            width: 300,
-            color: Colors.yellow,
-          ),
-        ]),
+        child: ListWheelScrollView(
+          itemExtent: 200,
+          diameterRatio: 2.0,
+          perspective: 0.005,
+          useMagnifier: true,
+          magnification: 1.2,
+          offAxisFraction: -0.5,
+          children: [
+            Container(
+              width: 200,
+              color: Colors.red,
+            ),
+            Container(
+              width: 70,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 200,
+              color: Colors.green,
+            ),
+            Container(
+              width: 300,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 200,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 50,
+              color: Colors.green,
+            ),
+            Container(
+              width: 300,
+              color: Colors.yellow,
+            ),
+          ],
+        ),
       ),
     );
   }
